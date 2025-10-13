89aa7eaa97b5e106673c46621a3af66eb61baa19
kihochalong <41330247@nfu.edu.tw>
2025-10-13 15:47:10 +0800

    Update main.cpp
    Add text Hello World

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
@@ -15,5 +16,7 @@ int main(int argc, char *argv[])
     // If you do not need a running Qt event loop, remove the call
     // to a.exec() or use the Non-Qt Plain C++ Application template.
 
+    std::cout <<"Hello World"<< std::endl;
+    system("pause");
     return a.exec();
 }
